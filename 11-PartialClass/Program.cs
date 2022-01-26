929529c04e4ba1aca40941023e54d4f5beef967b
mayardes <mayardesoliveira@gmail.com>
2022-01-25 20:58:59 -0400

    feat: remembering about partial classes - separete rules but time compilation these patial classes become one - use keywork partial class

---
+++ 11-PartialClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_PartialClass
+{
+    public partial class User
+    {
+        private int _code { get; set; }
+        private string _name { get; set; }
+        private string _password { get; set; }
+
+        public User(int code, string name, string password)
+        {
+            _code = code;
+            _name = name;
+            _password = password;
+        }
+    }
+
+    public partial class User
+    {
+        public string  GetName()
+        {
+            return this._name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User(122, "Mayardes", "123");
+            Console.WriteLine(user.GetName());
+        }
+    }
+}
