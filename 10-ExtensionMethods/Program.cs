bc7f5884d91f1b784f634320bdda554c93c0349a
mayardes <mayardesoliveira@gmail.com>
2022-01-25 20:46:21 -0400

    feat: remembering about extension methods -  always with use static classes -  for create new method, must to be use static methods too and use keyword THIS for inject

---
+++ 10-ExtensionMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ExtensionMethods
+{
+    public static class MyExtension
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Count();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "mayardes";
+            Console.WriteLine(name.Count());
+        }
+    }
+}
