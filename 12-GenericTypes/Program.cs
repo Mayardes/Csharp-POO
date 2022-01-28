80eb365f122b3832d24cb10be5a6445ad76362e3
mayardes <mayardesoliveira@gmail.com>
2022-01-27 21:59:05 -0400

    learning about generic types - generic types allows define in same method with differents parameter types, reusing same method <T> where T is a data type

---
+++ 12-GenericTypes/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _12_GenericTypes
+{
+    public class Stack<T>
+    {
+        int pos = 0;
+        T[] p = new T[10];
+        public void Push(T item)
+        {
+            p[pos] = item;
+            pos++;
+        }
+
+        public T Pop()
+        {
+            pos--;
+            return p[pos];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack1 = new Stack<string>();
+            stack1.Push("arg1");
+            stack1.Push("arg2");
+            stack1.Push("arg3");
+            stack1.Pop();
+            stack1.Pop();
+            stack1.Pop();
+
+            var stack2 = new Stack<bool>();
+            stack2.Push(false);
+            stack2.Push(true);
+
+            stack2.Pop();
+            stack2.Pop();
+
+
+
+
+        }
+    }
+}
