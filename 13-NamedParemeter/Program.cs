ac0b00b3256e789e335aa5347c0ed941ea044065
mayardes <mayardesoliveira@gmail.com>
2022-01-27 22:09:33 -0400

    feat: remembering about named parameter - change order position in parameter - organization in parameter list on method

---
+++ 13-NamedParemeter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _13_NamedParemeter
+{
+    public class DialogBox
+    {
+        public void MessageBox(string message, bool isActive, int position)
+        {
+            Console.WriteLine(message + isActive + position);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DialogBox dialogBox = new DialogBox();
+            dialogBox.MessageBox(isActive: true, position: 1, message: "hello");
+        }
+    }
+}
