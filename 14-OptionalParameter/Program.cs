da5c9fb54a21d39e642257de3f7d2cfdef21186e
mayardes <mayardesoliveira@gmail.com>
2022-01-27 22:17:57 -0400

    remembering about optional parameter

---
+++ 14-OptionalParameter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_OptionalParameter
+{
+    public class Student
+    {
+        public void ToStudy(string subject, string nameStudent ="")
+        {
+            Console.WriteLine(subject + nameStudent);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student();
+
+            student.ToStudy("CSharp");
+            student.ToStudy("JavaScript","Mayardes");
+        }
+    }
+}
