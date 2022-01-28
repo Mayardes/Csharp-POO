a08b6c8160b38ab9e3cc660b9cfec41fc84c785a
mayardes <mayardesoliveira@gmail.com>
2022-01-27 22:57:14 -0400

    remembering about overload methods - how to use differents types parameter in same method

---
+++ 15-OverloadsMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_OverloadsMethods
+{
+    public class Bank
+    {
+        public double Withdraw(double value)
+        {
+            return value;
+        }
+
+        public double Withdraw(string value)
+        {
+            return 3.40;
+        }
+
+        public double Withdraw(double value, string cpf)
+        {
+            return value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank =new Bank();
+            bank.Withdraw("90998375268");
+            bank.Withdraw(840.0);
+        }
+    }
+}
