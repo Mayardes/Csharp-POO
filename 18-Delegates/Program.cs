ab518bd78ae41a3bf628d019eb4610c598b3104a
mayardes <mayardesoliveira@gmail.com>
2022-01-29 13:56:07 -0400

    remebering about delegates - how to delegate an task for another method. - instance an object and this object call an method responsible or some action

---
+++ 18-Delegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Delegates
+{
+    public class BankSlip
+    {
+        public void ToPay(double value)
+        {
+            Console.WriteLine("Ticket payment in value of " + value);
+        }
+
+    }
+    public class Order
+    {
+        private BankSlip _bankSlip;
+
+        public Order()
+        {
+            this._bankSlip = new BankSlip();
+        }
+        public void Close(double value)
+        {
+            //using delegates
+            this._bankSlip.ToPay(value);
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankSlip bankSlip = new BankSlip();
+            bankSlip.ToPay(120);
+
+            Order order = new Order();
+            order.Close(150);
+        }
+    }
+}
