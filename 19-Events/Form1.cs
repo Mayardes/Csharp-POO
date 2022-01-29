1b2d9714f7b69d463c902a2a87fa69e3d6722b34
mayardes <mayardesoliveira@gmail.com>
2022-01-29 14:08:15 -0400

    remembering about events - what is event handler - what is multicast event

---
+++ 19-Events/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19_Events
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World 1");
+        }
+        private void button1_Click2(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World 2");
+        }
+        private void button1_Click3(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World 3");
+        }
+    }
+}
