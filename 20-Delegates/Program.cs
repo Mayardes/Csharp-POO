5ec94ef3b8296700e2ea5211028ab6c0bfe16d71
mayardes <mayardesoliveira@gmail.com>
2022-02-01 14:45:25 -0400

    feat: practicing about delegates - how to use delegate using events - how to pass a method as parameter - how to use anonymous function

--- 20-Delegates/Program.cs
+++ 20-Delegates/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace _20_Delegates
 {
+    public delegate void DelegateFunction(double balance);
+
+    public class ToPay
+    {
+        public event DelegateFunction EventFunction;
+
+        public void Init(DelegateFunction delegates)
+        {
+            delegates(25);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            ToPay topay = new ToPay();
+
+            topay.Init(valor => Console.WriteLine("thicket payment in value of "+ valor));
+        }
+
+        private static void Topay_EventFunction(double balance)
+        {
+            Console.WriteLine("Ticket payment value is " + balance);
         }
     }
 }
