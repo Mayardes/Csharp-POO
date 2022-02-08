a9061b237fdba1c02888ca618da4640cbb881178
mayardes <mayardesoliveira@gmail.com>
2022-02-07 23:30:43 -0400

    feat: working with throw exception - learning how to throw an exception in chain

---
+++ 23-TrainingThrowExceptions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _23_TrainingThrowExceptions
+{
+    class Program
+    {
+        public static void Intern(string activity)
+        {
+
+            var date = Convert.ToDateTime(activity);
+
+        }
+        private static void Programmer(string activity)
+        {
+            Intern(activity);
+        }
+        private static void Architect(string activity)
+        {
+            try
+            {
+                Programmer(activity);
+            }
+            catch (Exception E)
+            {
+                throw new Exception("Occured an error by Architect");
+            }
+        }
+        private static void Coordinator(string activity)
+        {
+            Architect(activity);
+        }
+        private static void Manager(string activity)
+        {
+            try
+            {
+                Coordinator(activity);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string activity = "99/99/99999";
+            Manager(activity);
+            Console.ReadLine();
+
+        }
+    }
+}
