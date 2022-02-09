e5cd355e9ee4aa0b25b87b3c31d56762727031ac
mayardes <mayardesoliveira@gmail.com>
2022-02-09 11:16:26 -0400

    feat: using interface IDisposable - creating a class inheriting from IDisposable - simulating method Dispose

---
+++ 27-InterfacesIDisposable/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace _27_InterfacesIDisposable
+{
+    public class Connection : IDisposable
+    {
+        public string Handle; //its same that a pointer for an resource
+        public void Dispose()
+        {
+            this.Handle = "";
+            Console.WriteLine("Resource released successfully");
+        }
+
+        public Connection()
+        {
+            this.Handle = "Resource Allocated";
+            Console.WriteLine("Resource allocated sucessfully");
+
+        }
+
+        public void Close()
+        {
+            Console.WriteLine("Closing connection...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var con = new Connection())
+            {
+                Console.WriteLine("Open connection..");
+                con.Close();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void TestConnection()
+        {
+            var obj = new SqlConnection();
+            try
+            {
+
+            }
+            catch (Exception E)
+            {
+
+            }
+            finally
+            {
+                if (obj != null)
+                    obj.Dispose();
+
+            }
+
+            //using(var obj2 = new SqlConnection())
+            //{
+
+            //}//call method dispose automatically
+        }
+    }
+}
