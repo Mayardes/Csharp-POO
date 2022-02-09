54af47adc5909d5783b6e394b93f2671fda7813c
mayardes <mayardesoliveira@gmail.com>
2022-02-08 23:19:19 -0400

    feat: working with abstract class and interfaces - making an interface to inherit from several interfaces and one abstract class - look up difference between interfaces and abstract class

---
+++ 25-AbstractClassesAndInterfaces/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _25_AbstractClassesAndInterfaces
+{
+    public interface IDBConnection
+    {
+        void Open();
+        void Close();
+        void StartTransaction();
+    }
+    public abstract class DBConnection : IDBConnection
+    {
+        public string ConnectionString { get; set; }
+        public virtual void Open()
+        {
+            Console.WriteLine("Opening connection..");
+            Console.WriteLine("Connected on " + this.ConnectionString);
+        }
+        public void Close()
+        {
+            Console.WriteLine("Closing conection...");
+        }
+        public void StartTransaction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class OracleConnection : DBConnection
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Opening new connection..");
+            Console.WriteLine("Connected on " + this.ConnectionString);
+        }
+    }
+    public class SqlConnection : DBConnection
+    {
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //could to use a factory method, or abstract factory
+            IDBConnection con = new OracleConnection()
+            {
+                ConnectionString = "Oracle"
+            };
+
+            con.Open();
+            con.Close();
+
+        }
+    }
+}
