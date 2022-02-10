949a2bc583899f34aa777aed2388245d5b985c29
mayardes <mayardesoliveira@gmail.com>
2022-02-10 12:59:51 -0400

    feat: working with collection (arraylist)

---
+++ 29-Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var team = new ArrayList();
+            team.Add("Oliveira");
+            team.Add("Souza");
+            team.Add(10);
+            team.Add(true);
+            team.Add(new StringBuilder());
+
+            foreach(object item in team)
+                Console.WriteLine(item);
+
+            Console.WriteLine(team.Capacity);
+            Console.WriteLine(team.Count);
+            var texto = (team[4] as StringBuilder).Append("teste");
+            Console.WriteLine(team[4]);
+            Console.ReadLine();
+        }
+    }
+}
