2f32b35c12c2316f979ff94b590ef1f91f23aeb2
mayardes <mayardesoliveira@gmail.com>
2022-02-13 15:07:03 -0400

    feat: a little about knowledge of iheritance and making casting - how to recieve a base type and making type casting (warning to use)

---
+++ 34-TypeCasting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _34_TypeCasting
+{
+    public class Person
+    {
+        public string Name;
+    }
+    public class IndividualPerson : Person
+    {
+        public string CPF;
+    }
+    public class LegalPerson : Person
+    {
+        public string CNPJ;
+    }
+
+    public static class ReportPeople
+    {
+        public static void Print(Person person)
+        {
+            if(person is IndividualPerson)
+                Console.WriteLine((person as IndividualPerson).Name);
+            if(person is LegalPerson)
+                Console.WriteLine((person as LegalPerson).Name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = new IndividualPerson()
+            {
+                CPF = "445211212",
+                Name = "Matthew"
+            };
+
+            var lp = new LegalPerson()
+            {
+                CNPJ = "9498489/0001-2",
+                Name = "Company INC"
+            };
+
+            ReportPeople.Print(ip);
+            ReportPeople.Print(lp);
+        }
+    }
+}
