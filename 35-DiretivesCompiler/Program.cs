04370910a8739ea068311e68fee15bfd11d6a639
mayardes <mayardesoliveira@gmail.com>
2022-02-14 12:30:51 -0400

    feat: directives pre-proccessing

---
+++ 35-DiretivesCompiler/Program.cs
@@ -0,0 +1,17 @@
+﻿#define OK
+using System;
+
+namespace _35_DiretivesCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if (OK)
+            Console.WriteLine("OK");
+#elif OK
+            Console.WriteLine("OK2");
+#endif
+        }
+    }
+}
