18e73c713bdc3058be459f52c5de860076aa9c26
mayardes <mayardesoliveira@gmail.com>
2022-02-16 11:15:38 -0400

    feat: simples exemple of LINQ

---
+++ 38-Linq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            var pars = (from n in numbers
+                         where (n % 2) == 0
+                         select n);
+
+            foreach(var item in pars)
+                Console.WriteLine(item);
+                    
+        }
+    }
+}
