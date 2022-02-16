5c0ded7005fa4d41f20f4f0e2e1f3075f8cee975
mayardes <mayardesoliveira@gmail.com>
2022-02-16 13:10:05 -0400

    feat: first exemple from LINQ

---
+++ 39-ExempleLINQ6/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace _39_ExempleLINQ6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ToQuery_Click(object sender, EventArgs e)
+        {
+            var query = from file in Directory.GetFiles(@"C:\Windows\System32")
+                        select new
+                        {
+                            FileName = Path.GetFileName(file),
+                            Extension = Path.GetExtension(file)
+                        };
+             
+            GridView1.DataSource = query;
+            GridView1.DataBind();
+
+        }
+    }
+}
