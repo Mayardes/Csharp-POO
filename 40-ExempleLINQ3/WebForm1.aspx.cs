4f3261e08bcb8269e5a9128931d636dd5f4e226a
mayardes <mayardesoliveira@gmail.com>
2022-02-16 22:33:53 -0400

    feat: working with expression LINQ

---
+++ 40-ExempleLINQ3/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _40_ExempleLINQ3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnQuery_Click(object sender, EventArgs e)
+        {
+            var query = from item in Directory.GetFiles(@"C:\Windows\System32")
+                        let fileName = Path.GetFileName(item)
+                        let extension = Path.GetExtension(item).ToUpper()
+
+                        orderby fileName, extension descending
+                        
+                        select new
+                        {
+                            FileName = fileName,
+                            Extension = extension
+                        };
+
+            GridView1.DataSource = query;
+            GridView1.DataBind();
+
+                
+        }
+    }
+}
