2af40b05d6a19e685d112436db82d3d8c71dd096
mayardes <mayardesoliveira@gmail.com>
2022-02-17 10:16:37 -0400

    feat: working expression LINQ - working statements where

---
+++ 41-ExempleLINQ6/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_41_ExempleLINQ6.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Button ID="btnQuery" runat="server" OnClick="btnQuery_Click" Text="Query" />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ 41-ExempleLINQ6/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _41_ExempleLINQ6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnQuery_Click(object sender, EventArgs e)
+        {
+            var query = from item in Directory.GetFiles(@"C:\Windows\System32")
+                        let infoFile = new FileInfo(item)
+                        let sizeFile = infoFile.Length /1024M /1024M
+                        where sizeFile > 1M && infoFile.Extension.ToUpper() == ".EXE"
+
+                        select new
+                        {
+                            FileName = infoFile.Name,
+                            SizeFile = sizeFile
+                        };
+
+            GridView1.DataSource = query;
+            GridView1.DataBind();
+        }
+    }
+}
