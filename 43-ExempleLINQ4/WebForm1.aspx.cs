b92d6f7686bcaefc578e50d21a0d7bdbd247a3d3
mayardes <mayardesoliveira@gmail.com>
2022-02-17 17:15:32 -0400

    feat: working with linq -using compose aggroupments1

---
+++ 43-ExempleLINQ4/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace _43_ExempleLINQ4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void brnQuery_Click(object sender, EventArgs e)
+        {
+            var query = from item in Directory.GetFiles(@"C:\Windows\System32")
+                        let infoFile = new FileInfo(item)
+                        group infoFile by new
+                        {
+                            Folder = infoFile.DirectoryName,
+                            Extension = infoFile.Extension.ToUpper(),
+                        }
+                        into groupedByFolderAndExtension
+
+                        let amount = groupedByFolderAndExtension.Count()
+                        let sizeTotalKB = groupedByFolderAndExtension.Sum(x => x.Length)
+
+                        orderby groupedByFolderAndExtension.Key.Folder ascending, sizeTotalKB descending
+
+                        select new
+                        {
+                            Folder = groupedByFolderAndExtension.Key.Folder,
+                            Amount = amount,
+                            SizeTotal = sizeTotalKB,
+                            Extension = groupedByFolderAndExtension.Key.Extension
+                        };
+
+            GridView1.DataSource = query;
+            GridView1.DataBind();
+
+        }
+    }
+}
